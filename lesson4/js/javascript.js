9fde52c0ed6b27df2cfa4d75085fc0980e10daad
wallylime <82895781+wallylime@users.noreply.github.com>
2021-05-18 20:25:57 -0600

    2nd draft

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,20 +1,18 @@
-//Building day name and month name lists
+//Navigation display function for smaller screens
+function toggleMenu() {
+  document.getElementById("navigation").classList.toggle("hide");
+}
+
+//Get the current date for the footer
+let today = new Date();
+
 const days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
 const months = [January, February, March, April, May, June, July, August, September, October, November, December]
-//Getting the current date
-let today = new Date();
+
 let day = today.getDay();
 let month = today.getMonth();
 let date = today.getDate();
 let year = today.getFullYear();
 let currentDate = days[day] + ", " + date + " " + months[month] + " " + year;
-document.getElementById("currentDate").innerHTML = currentDate;
-document.getElementById("year").innerHTML = year;
-
-//Navigation display function for smaller screens
-const menu = document.querySelector(".menu");
-const nav = document.querySelector(".navigation");
-menu.addEventListener("click", () =>
-{nav.classList.toggle("responsive")}, false);
-window.onresize = () => {if (window.innerWidth > 760)
-nav.classList.remove("responsive")};
+document.querySelector("#year").textContent = year;
+document.querySelector("#currentDate").textContent = currentDate;
