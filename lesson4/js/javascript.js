f38d3cc1d13d17fe86995e1291c960842f0534e4
wallylime <82895781+wallylime@users.noreply.github.com>
2021-05-20 13:47:30 -0600

    changes

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -3,16 +3,11 @@ function toggleMenu() {
   document.getElementById("navigation").classList.toggle("hide");
 }
 
-//Get the current date for the footer
+//Building day and month lists
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//Getting current date info
 let today = new Date();
-
-const days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
-const months = [January, February, March, April, May, June, July, August, September, October, November, December]
-
-let day = today.getDay();
-let month = today.getMonth();
-let date = today.getDate();
-let year = today.getFullYear();
-let currentDate = days[day] + ", " + date + " " + months[month] + " " + year;
-document.querySelector("#year").textContent = year;
-document.querySelector("#currentDate").textContent = currentDate;
+let dateDisplay = days[today.getDay()] + ", " + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+document.querySelector("#year").textContent = today.getFullYear();
+document.querySelector("#currentDate").textContent = dateDisplay;
---
+++ lesson5/js/bom.js
@@ -0,0 +1,3 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector(".list");
