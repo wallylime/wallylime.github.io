29b325b1bbf1fe78f4a4733ecae2af94e794cd02
wallylime <82895781+wallylime@users.noreply.github.com>
2021-06-03 13:11:36 -0600

    fixes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,7 @@
-var current_temp = parseFloat(document.querySelector("#current-temp").value);
-var wind_speed = parseFloat(document.querySelector("#wind-speed").value);
+var current_temp = parseFloat(document.querySelector("#current-temp").innerHTML);
+var wind_speed = parseFloat(document.querySelector("#wind-speed").innerHTML);
 if (current_temp < 51 && wind_speed > 3) {
-  var wind_chill = 35.74 + .6215 * tempF - 35.75 * Math.pow(speed, .16) + .4275 * tempF * Math.pow(speed, .16);
+  var wind_chill = 35.74 + .6215 * current_temp - 35.75 * Math.pow(speed, .16) + .4275 * current_temp * Math.pow(speed, .16);
   wind_chill = Math.round(wind_chill * 10) / 10;
   document.querySelector("#wind-chill").innerHTML = wind_chill + "&deg;F";
 } 
--- lesson7/js/javascript.js
+++ lesson7/js/javascript.js
@@ -1,1 +1,32 @@
-cons
+//Makes a list of all "img" elements with the attribute "data-src" and stores them in the constant allImages
+const allImages = document.querySelectorAll("img[data-src]");
+
+const loadImages = (img) => {
+  img.setAttribute("src", img.getAttribute("data-src"));
+  img.onload = () => {
+    img.removeAttribute("data-src");
+  }
+}
+
+allImages.forEach((img) => {
+  loadImages(img);
+})
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  allImages.forEach((img) => {
+    observer.observe(img);
+  });
+}
+else {
+  allImages.forEach((img) => {
+    loadImages(img);
+  });
+}
