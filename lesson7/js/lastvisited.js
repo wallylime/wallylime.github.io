6f1d4efebcbdd18737b617d7152d0f135b1c3969
wallylime <82895781+wallylime@users.noreply.github.com>
2021-06-04 11:15:06 -0600

    date

--- lesson7/js/lastvisited.js
+++ lesson7/js/lastvisited.js
@@ -1,7 +1,7 @@
 //Get the day/time of current visit in order to store as value for "lastVisited" key in local storage 
 let today = new Date();
 let time = today.getTime();
-console.log(time);
+console.log(today);
 
 //Check for last visited in local storage
 // if (!localStorage.getItem("lastVisited")) {
