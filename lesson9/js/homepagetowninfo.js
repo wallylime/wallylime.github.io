9d3ed509fae597185eaf1697da2ae72069871c6f
wallylime <82895781+wallylime@users.noreply.github.com>
2021-06-18 14:27:55 -0600

    home page

--- lesson9/js/homepagetowninfo.js
+++ lesson9/js/homepagetowninfo.js
@@ -10,82 +10,88 @@ fetch(townListURL)
     
     //Soda Springs Info
       let townInfo = document.createElement("section");
+      let datadiv = document.createElement("div");
 
       let h2 = document.createElement("h2");
       h2.textContent = towns[0].name;
-      townInfo.appendChild(h2);
 
       let p1 = document.createElement("p");
       p1.innerHTML = `<span class="infoHeadings">Year Founded: </span> ${towns[0].yearFounded}`;
-      townInfo.appendChild(p1);
 
       let p2 = document.createElement("p");
       p2.innerHTML = `<span class="infoHeadings">Population: </span> ${towns[0].currentPopulation}`;
-      townInfo.appendChild(p2);
 
       let p3 = document.createElement("p");
       p3.innerHTML = `<span class="infoHeadings">Annual Rain Fall: </span> ${towns[0].averageRainfall}`;
-      townInfo.appendChild(p3);
+      
+      datadiv.append(h2, p1, p2, p3);
+      datadiv.setAttribute("class", "data");
 
       let image = document.createElement("img");
       image.setAttribute("src", "images/floristshop.jpg");
       image.setAttribute("alt", "picture of a florist shop by Krisztina Papp");
-      image.setAttribute("title", "Photo by Krisztina Papp on Unsplash")
-      townInfo.appendChild(image);
+      image.setAttribute("title", "Photo by Krisztina Papp on Unsplash");
+
+      townInfo.append(datadiv, image);
+      townInfo.setAttribute("class", "towngrid");
 
       document.querySelector("div.towns").appendChild(townInfo);
 
     //Fish Haven Info
       townInfo = document.createElement("section");
+      datadiv = document.createElement("div");
 
       h2 = document.createElement("h2");
       h2.textContent = towns[2].name;
-      townInfo.appendChild(h2);
 
       p1 = document.createElement("p");
       p1.innerHTML = `<span class="infoHeadings">Year Founded: </span> ${towns[2].yearFounded}`;
-      townInfo.appendChild(p1);
 
       p2 = document.createElement("p");
       p2.innerHTML = `<span class="infoHeadings">Population: </span> ${towns[2].currentPopulation}`;
-      townInfo.appendChild(p2);
 
       p3 = document.createElement("p");
       p3.innerHTML = `<span class="infoHeadings">Annual Rain Fall: </span> ${towns[2].averageRainfall}`;
-      townInfo.appendChild(p3);
+
+      datadiv.append(h2, p1, p2, p3);
+      datadiv.setAttribute("class", "data");
 
       image = document.createElement("img");
       image.setAttribute("src", "images/housebylake.jpg");
       image.setAttribute("alt", "picture of a house by a lake by Kristina Tamašauskaitė");
       image.setAttribute("title", "Photo by Kristina Tamašauskaitė on Unsplash");
-      townInfo.appendChild(image);
+
+      townInfo.append(datadiv, image);
+      townInfo.setAttribute("class", "middlegrid");
 
       document.querySelector("div.towns").appendChild(townInfo);
 
     //Preston Info
       townInfo = document.createElement("section");
+      datadiv = document.createElement("div");
 
       h2 = document.createElement("h2");
       h2.textContent = towns[6].name;
-      townInfo.appendChild(h2);
 
       p1 = document.createElement("p");
       p1.innerHTML = `<span class="infoHeadings">Year Founded: </span> ${towns[6].yearFounded}`;
-      townInfo.appendChild(p1);
 
       p2 = document.createElement("p");
       p2.innerHTML = `<span class="infoHeadings">Population: </span> ${towns[6].currentPopulation}`;
-      townInfo.appendChild(p2);
 
       p3 = document.createElement("p");
       p3.innerHTML = `<span class="infoHeadings">Annual Rain Fall: </span> ${towns[6].averageRainfall}`;
-      townInfo.appendChild(p3);
+      
+      datadiv.append(h2, p1, p2, p3);
+      datadiv.setAttribute("class", "data");
 
       image = document.createElement("img");
       image.setAttribute("src", "images/cityroad.jpg");
       image.setAttribute("alt", "picture of a road going through a small town by Louise Tollisen");
       image.setAttribute("title", "Photo by Louise Tollisen on Unsplash");
-      townInfo.appendChild(image);
+
+      townInfo.append(datadiv, image);
+      townInfo.setAttribute("class", "towngrid");
 
       document.querySelector("div.towns").appendChild(townInfo);
     })
