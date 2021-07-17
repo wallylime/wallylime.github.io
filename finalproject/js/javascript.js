84ddca7f569b23b66e191f9cf42250f2571ae0a5
wallylime <82895781+wallylime@users.noreply.github.com>
2021-07-16 19:41:18 -0600

    finalprojectrevisions

--- finalproject/js/javascript.js
+++ finalproject/js/javascript.js
@@ -2,3 +2,12 @@
 function toggleMenu() {
   document.querySelector("#navigation").classList.toggle("hide");
 }
+
+//Last updated & copyright year for footer
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let today = new Date();
+document.getElementById("year").textContent = today.getFullYear();
+
+let lastUpdate = new Date(document.lastModified);
+document.getElementById("update").textContent = `${months[lastUpdate.getMonth()]} ${lastUpdate.getDate()}, ${lastUpdate.getFullYear()}`;
--- finalproject/js/weather.js
+++ finalproject/js/weather.js
@@ -2,32 +2,24 @@ const weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=43.6121&
 fetch(weatherURL)
   .then((response) => response.json())
   .then((meridianWeather) => {
-    //For current weather info
-    let imgurl = "https://openweathermap.org/img/wn/";
-    let icon = document.getElementById("weather-icon");
-    icon.setAttribute("src", imgurl + meridianWeather.current.weather[0].icon + "@2x.png");
-    icon.setAttribute("alt", meridianWeather.current.weather[0].description + " icon");
-    
+    //For current weather info    
     let currentWeather = document.getElementById("current-weather");
-    currentWeather.innerHTML += `${meridianWeather.current.weather[0].description.toUpperCase()} <br> <span class="weather-numbers">${meridianWeather.current.temp}&deg; F</span> <br> HUMIDITY: <span class="weather-numbers">${meridianWeather.current.humidity}%</span>`;
+    currentWeather.innerHTML = `<span class="weather-words">${meridianWeather.current.weather[0].description.toUpperCase()} & </span><span class="weather-numbers">${meridianWeather.current.temp}&deg; F</span> <br> <span class="weather-words">HUMIDITY: </span><span class="weather-numbers">${meridianWeather.current.humidity}%</span>`;
     
-    let weatherAlert = meridianWeather.alerts[0].description;
-    alert(weatherAlert);
-
     //For three day forecast
     const dayofweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let num = 1;
-    //The 7-day forecast starts with the current day, so I am using the days indexed 1-3 to get the next 3 days
     for (i=1; i < 4; i++) {
-      //Here's the code that I thought should work:
-      let date = new Date(meridianWeather.daily[i].dt);
-      let forecast = document.getElementById("forecast-" + num);
-      forecast.innerHTML = `<span class="forecastDay">${dayofweek[date.getDay()]}</span> <br> <span class="weather-numbers">${meridianWeather.daily[i].temp.day}&deg; F</span>`;
+      let date = new Date(meridianWeather.daily[i].dt * 1000);
+      let forecast = document.getElementById("forecast" + num);
+      forecast.innerHTML += `<span class="weather-words">${dayofweek[date.getDay()].toUpperCase()}: <br> </span><span class="weather-numbers">${meridianWeather.daily[i].temp.day}&deg; F</span><br>`;
       num += 1;
-      //But for some reason, it keeps giving me Monday as the day of the week for each day. I used the console to troubleshoot:
-      console.log(meridianWeather.daily[i].dt);
-      console.log(date);
-      //Each timestamp is showing up in the console correctly and when I use an online timestamp converter tool, it shows the correct date. HOWEVER, when the console shows the date variable, it shows up as Mon, Jan 19, 1970. I have no idea what is going on or what I am doing wrong. I thought I understood how this stuff worked. Apparently not.
+    }
+
+    //Remember to check that this works on a day with a weather alert
+
+    if (meridianWeather.hasOwnProperty("alerts[0].description") == true) {
+      alert(meridianWeather.alerts[0].description);
     }
 
   })
