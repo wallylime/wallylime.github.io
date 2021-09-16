49a357ff9f0d69b0bf1dba6462b5d117b74a9c2e
wallylime <82895781+wallylime@users.noreply.github.com>
2021-09-16 10:47:16 -0600

    Cleaning up and managing portfolio

--- lesson3/js/javascript.js
+++
@@ -1,5 +0,0 @@
-// get current year
-let date = new Date();
-document.querySelector("#year").textContent = date.getFullYear();
-//get last modified date and time
-document.querySelector("#lastmodified").textContent = document.lastModified;
