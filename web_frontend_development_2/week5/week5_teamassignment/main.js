8c2e4e86b7a9042e2978265c53197bc98108e0f1
wallylime <82895781+wallylime@users.noreply.github.com>
2021-10-14 11:52:52 -0600

    team work

--- web_frontend_development_2/week5/week5_teamassignment/main.js
+++ web_frontend_development_2/week5/week5_teamassignment/main.js
@@ -1,73 +1,58 @@
+import Hikes from './hikes.js';
+const hike = new Hikes(document.getElementById('hikes'));
+console.log(hike.getAllHikes());
 
-
-//create an array of hikes
-const hikeList = [
-  {
-    name: "Bechler Falls",
-    imgSrc: "falls.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "3 miles",
-    difficulty: "Easy",
-    description:
-      "Beautiful short hike along the Bechler river to Bechler Falls",
-    directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
-  },
-  {
-    name: "Teton Canyon",
-    imgSrc: "falls.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "3 miles",
-    difficulty: "Easy",
-    description: "Beautiful short (or long) hike through Teton Canyon.",
-    directions:
-      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
-  },
-  {
-    name: "Denanda Falls",
-    imgSrc: "falls.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "7 miles",
-    difficulty: "Moderate",
-    description:
-      "Beautiful hike through Bechler meadows river to Denanda Falls",
-    directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
-  }
-];
-
-const imgBasePath = "//byui-cit.github.io/cit261/examples/";
-//on load grab the array and insert it into the page
-window.addEventListener("load", () => {
-  showHikeList();
-});
-
-function showHikeList() {
-  const hikeListElement = document.getElementById("hikes");
+window.addEventListener('load', () => {
+  // 
+  const hikeListElement = document.getElementById('hikes');
   hikeListElement.innerHTML = "";
-  renderHikeList(hikeList, hikeListElement);
-}
-
-function renderHikeList(hikes, parent) {
+  renderHikeList(hikeListElement, hike.getAllHikes());
+});
+  
+// methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
+function renderHikeList(parent, hikes) {
   hikes.forEach(hike => {
-    parent.appendChild(renderOneHike(hike));
+    parent.appendChild(renderOneHikeLight(hike));
   });
 }
-function renderOneHike(hike) {
+
+function renderOneHikeLight(hike) {
   const item = document.createElement("li");
-
   item.innerHTML = ` <h2>${hike.name}</h2>
-        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-        <div>
-                <div>
-                    <h3>Distance</h3>
-                    <p>${hike.distance}</p>
-                </div>
-                <div>
-                    <h3>Difficulty</h3>
-                    <p>${hike.difficulty}</p>
-                </div>
-        </div>`;
-
+  <div class="image"><img src="https://byui-cit.github.io/cit261/examples/${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+  <div>
+          <div>
+              <h3>Distance</h3>
+              <p>${hike.distance}</p>
+          </div>
+          <div>
+              <h3>Difficulty</h3>
+              <p>${hike.difficulty}</p>
+          </div>
+  </div>`;
+  return item;
+}
+function renderOneHikeFull(hike) {
+  const item = document.createElement("li");
+  item.innerHTML = ` <h2>${hike.name}</h2>
+  <div class="image"><img src="https://byui-cit.github.io/cit261/examples/${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+  <div>
+          <div>
+              <h3>Distance</h3>
+              <p>${hike.distance}</p>
+          </div>
+          <div>
+              <h3>Difficulty</h3>
+              <p>${hike.difficulty}</p>
+          </div>
+          <div>
+            <h3>Description</h3>
+            <p>${hike.description}</p>
+          </div>
+          <div>
+            <h3>Directions</h3>
+            <p>${hike.directions}</p>
+        </div>
+  </div>`;
   return item;
 }
