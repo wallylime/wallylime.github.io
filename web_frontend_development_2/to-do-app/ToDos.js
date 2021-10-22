b8dd02a8fca360776625af55bf4fb86cca67c117
wallylime <82895781+wallylime@users.noreply.github.com>
2021-10-21 21:15:00 -0600

    delete template thingy

--- web_frontend_development_2/to-do-app/ToDos.js
+++ web_frontend_development_2/to-do-app/ToDos.js
@@ -1,18 +1,35 @@
-const listItems = document.querySelectorAll("li p");
+const toDoList = document.querySelector("ul");
+const listItems = toDoList.querySelectorAll("li");
 
 //Event listener for crossing items off 
-listItems.forEach(listItem => addEventListener("dblclick", crossOffItem));
+listItems.forEach(listItem => addEventListener("click", crossOffItem));
 
+//Why is this also crossing off the paragraph?
 //This function crosses off a completed to-do item
 function crossOffItem(event) {
   event.target.classList.add("completed");
-  event.target.removeEventListener("dblclick", crossOffItem);
-  event.target.addEventListener("dblclick", test);
+  event.target.removeEventListener("click", crossOffItem);
+  // event.target.addEventListener("dblclick", uncrossOff);
 };
 
-function test(event) {
-  console.log("Testing 1,2,3");
-}  
+
+// function uncrossOff(event) {
+//   console.log("This part is working");
+//   console.log(event.target);
+//   //why isn't this removing "completed" from the classList?
+//   event.target.classList.remove("completed");
+//   console.log(event.target.classList);
+// };  
+
+// const removeButtons = document.getElementsByClassName("removeButton");
+
+// removeButtons.forEach(removeButton => addEventListener("dblclick", removeTask));
+
+//This function removes a list item
+// function removeTask(event) {
+//   let li = event.target.parentElement;
+//   li.remove();
+// }
 
 // listItems.forEach(listItem => {
 //     let completed = listItem.classList.includes("completed")
@@ -22,18 +39,7 @@ function test(event) {
 //   }
 // };
 
-
-/*
-//Event listener for the "remove" buttons
-var removeButtons = document.querySelectorAll(".removeButton");
-removeButtons.forEach(button => addEventListener ("dblclick", removeTask));
-
-//This function removes a list item
-function removeTask(event) {
-  event.target.classList.add("hide");
-}
-
-function addTask() {
+/*function addTask() {
   let toDoList = document.getElementById("js-toDoList");
   toDoList.appendChild(createLi());
 };
--- web_frontend_development_2/week3/event_listeners.js
+++ web_frontend_development_2/week3/event_listeners.js
@@ -46,7 +46,7 @@ onceParagraph.addEventListener('click', remove);
 function remove(event) {
     console.log('Enjoy this while it lasts!');
     onceParagraph.style.backgroundColor = 'pink';
-    onceParagraph.removeEventListener('click',remove);
+    onceParagraph.removeEventListener('click', remove);
 }
 
 const brokenLink = document.getElementById('broken');
