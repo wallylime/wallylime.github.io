4640e19094137ae8a776797603e99a0fc6906b85
wallylime <82895781+wallylime@users.noreply.github.com>
2021-10-21 19:35:10 -0600

    event listener fixes

--- web_frontend_development_2/to-do-app/ToDos.js
+++ web_frontend_development_2/to-do-app/ToDos.js
@@ -1,43 +1,55 @@
+const listItems = document.querySelectorAll("li p");
+
 //Event listener for crossing items off 
-var listItems = document.querySelectorAll("li p");
 listItems.forEach(listItem => addEventListener("dblclick", crossOffItem));
 
 //This function crosses off a completed to-do item
 function crossOffItem(event) {
   event.target.classList.add("completed");
-}
+  event.target.removeEventListener("dblclick", crossOffItem);
+  event.target.addEventListener("dblclick", test);
+};
 
+function test(event) {
+  console.log("Testing 1,2,3");
+}  
+
+// listItems.forEach(listItem => {
+//     let completed = listItem.classList.includes("completed")
+//     if (listItem !== completed) {
+//       listItem.classList.add("hide");
+//     }
+//   }
+// };
+
+
+/*
 //Event listener for the "remove" buttons
 var removeButtons = document.querySelectorAll(".removeButton");
-removeButtons.forEach(button => addEventListener ("click", removeTask));
+removeButtons.forEach(button => addEventListener ("dblclick", removeTask));
 
 //This function removes a list item
 function removeTask(event) {
-  event.target
+  event.target.classList.add("hide");
 }
 
-/*input: new task in input box
-  processing: create a new li with p and button, add the content from the input box to the p and append li to ul
-  output: have new task item appear in to-do list
+function addTask() {
+  let toDoList = document.getElementById("js-toDoList");
+  toDoList.appendChild(createLi());
+};
+
+This function creates a li with two children: p & button
+function createLi() {
+  let li = document.createElement("li");
+  let p = document.createElement("p");
+  //adds the "incomplete" star and the new list item
+  p.innerHTML = `<span class="star incomplete">&#9734;</span> ${document.getElementById("newTask").value}`;
+  //adds a remove button to each list item
+  let button = document.createElement("button");
+  button.setAttribute("class", "deleteButton");
+  button.setAttribute("onclick", "deleteItem()");
+  button.innerHTML = `<span class="x">X</span> Remove`;
+  li.append(p, button);
+  return li;}
 */
-
-// function addTask() {
-//   let toDoList = document.getElementById("js-toDoList");
-//   toDoList.appendChild(createLi());
-// };
-
-// //This function creates a li with two children: p & button
-// function createLi() {
-//   let li = document.createElement("li");
-//   let p = document.createElement("p");
-//   //adds the "incomplete" star and the new list item
-//   p.innerHTML = `<span class="star incomplete">&#9734;</span> ${document.getElementById("newTask").value}`;
-//   //adds a remove button to each list item
-//   let button = document.createElement("button");
-//   button.setAttribute("class", "deleteButton");
-//   button.setAttribute("onclick", "deleteItem()");
-//   button.innerHTML = `<span class="x">X</span> Remove`;
-//   li.append(p, button);
-//   return li;
-// }
 
--- web_frontend_development_2/week3/event_listeners.js
+++ web_frontend_development_2/week3/event_listeners.js
@@ -1,6 +1,6 @@
-// function doSomething(){
-//   console.log("Something happened!");
-// }
+function doSomething(){
+  console.log("Something happened!");
+}
 
 // function doSomething(event){
 //   console.log(event.type);
@@ -14,22 +14,22 @@
 //   console.log(`screen: (${event.screenX},${event.screenY}), page: (${event.pageX},${event.pageY}), client: (${event.screenX},${event.screenY})`)
 // }
 
-// addEventListener('click', doSomething);
+addEventListener('click', doSomething);
 
-// const clickParagraph = document.getElementById('click');
+const clickParagraph = document.getElementById('click');
 // clickParagraph.addEventListener('click',() => console.log('click') );
 // clickParagraph.addEventListener('mousedown',() => console.log('down') );
 // clickParagraph.addEventListener('mouseup',() => console.log('up') );
 
-// const dblclickParagraph = document.getElementById('dblclick');
-// dblclickParagraph.addEventListener('dblclick', highlight);
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
 function highlight(event){
     event.target.classList.toggle('highlight');
 }
 
-// const mouseParagraph = document.getElementById('mouse');
-// mouseParagraph.addEventListener('mouseover', highlight);
-// mouseParagraph.addEventListener('mouseout', highlight);
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
 
 // mouseParagraph.addEventListener('mousemove', () =>  console.log('You Moved!') );
 
@@ -41,19 +41,19 @@ function highlight(event){
 
 // addEventListener('keydown', (event) => console.log(`You pressed the ${event.key} character`));
 
-// const onceParagraph = document.getElementById('once');
-// onceParagraph.addEventListener('click', remove);
-// function remove(event) {
-//     console.log('Enjoy this while it lasts!');
-//     onceParagraph.style.backgroundColor = 'pink';
-//     onceParagraph.removeEventListener('click',remove);
-// }
+const onceParagraph = document.getElementById('once');
+onceParagraph.addEventListener('click', remove);
+function remove(event) {
+    console.log('Enjoy this while it lasts!');
+    onceParagraph.style.backgroundColor = 'pink';
+    onceParagraph.removeEventListener('click',remove);
+}
 
-// const brokenLink = document.getElementById('broken');
-// brokenLink.addEventListener('click',(event) => {
-//     event.preventDefault();
-//     console.log('Broken Link!');
-// });
+const brokenLink = document.getElementById('broken');
+brokenLink.addEventListener('click',(event) => {
+    event.preventDefault();
+    console.log('Broken Link!');
+});
 
 ulElement = document.getElementById('list');
 liElement = document.querySelector('#list li');
@@ -66,4 +66,4 @@ liElement = document.querySelector('#list li');
 // ulElement.addEventListener('click', (event) => console.log('Clicked on ul'),true);
 // liElement.addEventListener('click', (event) => console.log('Clicked on li'),true);
 
-ulElement.addEventListener('click',highlight);
+ulElement.addEventListener('click', highlight);
