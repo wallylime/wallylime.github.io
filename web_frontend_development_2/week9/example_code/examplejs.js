7bcd1b6348849e896485de13ed3a23e468dd09be
wallylime <82895781+wallylime@users.noreply.github.com>
2021-11-08 11:35:17 -0700

    reload go back

--- web_frontend_development_2/week9/example_code/examplejs.js
+++ web_frontend_development_2/week9/example_code/examplejs.js
@@ -3,8 +3,27 @@ window.alert("This page will be obnoxious!");
 window.confirm("Are you sure you want to be on this page?");
 const userName = window.prompt("Okay, what is your name?");
 const h2 = document.querySelector("h2");
-h2.innerHTML = `Hello, ${userName}! Welcome to this obnoxious page!`
+if (userName != "") {
+  h2.innerHTML = `Hello, ${userName}! Welcome to this obnoxious page!`
+}
+else {
+  h2.innerHTML = `Hello User! Welcome to this obnoxious page!`
+}
+
+function popUp() {
+  window.open("https://wallylime.github.io/web_frontend_development_2/week9/example_code/popup.html", "Popup", "top=50,width=300,height=300,resizable=yes");
+}
+
+window.setTimeout(animate, 3000);
+function animate() {
+  const square = document.querySelector("#square");
+  square.classList.add("animate");
+}
 
 function goBack() {
-  window.open("https://wallylime.github.io/web_frontend_development_2/week9/example_code/popup.html", "Popup", "top=50,width=200,height=200,resizable=yes");
+  window.history.go(-1);
+}
+
+function reload() {
+  window.history.go(0);
 }
